0e79abda4e85c22329906a2778a7e6d70df1b9ee
jorge <jorgechucena@gmail.com>
2017-01-09 19:42:46 +0100

    probar pluggin

--- maven1/src/main/java/egc/App.java
+++ maven1/src/main/java/egc/App.java
@@ -4,10 +4,13 @@ package egc;
  * Hello world!
  *
  */
+
+
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
     }
+//TODO tengo que crear nuevos métodos
 }
